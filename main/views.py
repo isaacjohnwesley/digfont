70d42e53a29524e88b67bb942fe79ffa57d8c76d
Isaac John Wesley <wonderviz@gmail.com>
2013-06-07 00:12:33 +0530

    adding some code in views.py

--- main/views.py
+++ main/views.py
@@ -1,6 +1,18 @@
 from flask import Blueprint, render_template
+import urllib2
+
+
 main = Blueprint('main', __name__, template_folder='pages')
+
+url = 'https://gocardless.com/'
 
 @main.route('/')
 def index():
+
+    req = urllib2.Request(url)
+    req.headers['Range'] = 'bytes=%s-%s' % (500, 1024)
+
+    contents = urllib2.urlopen(req).read()
+    print contents
+
     return render_template('index.html')
