fb7909de96fc5f1f0f0f852a016381af624c8095
Isaac John Wesley <wonderviz@gmail.com>
2013-07-02 17:54:02 +0530

    added the code for fetching css from a url

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask
+from flask.ext.assets import Environment
 from main.views import main
+from webassets.loaders import PythonLoader
 
 app = Flask(__name__)
+assets = Environment(app)
 app.register_blueprint(main, url_prefix='/')
 
 app.config.from_pyfile('settings.py')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
