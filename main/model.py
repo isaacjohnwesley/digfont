7f042bda1c99df7269833442f90210ccd77a3f71
Prasanna Venkadesh <prasmailme@gmail.com>
2013-10-22 21:44:11 +0530

    Sticking to PEP8 Style guide. Made run.py executable

--- main/model.py
+++ main/model.py
@@ -1,22 +1,28 @@
 import peewee
 
-#Database configurations
-DATABASE_NAME = '' # Please use your own local database name
-DATABASE_USER = '' # Please use your own local user name
-DATABASE_PASSWORD = '' # Please use your own local user name
-DATABASE_HOST = '' # Please use your own host
-DATABASE_PORT = '' # Please use your own port
+# Database configurations
+DATABASE_NAME = ''  # Please use your own local database name
+DATABASE_USER = ''  # Please use your own local user name
+DATABASE_PASSWORD = ''  # Please use your own local user name
+DATABASE_HOST = ''  # Please use your own host
+DATABASE_PORT = ''  # Please use your own port
 
-#Import secrets from the file the world should not see :P
+# Import secrets from the file the world should not see :P
 try:
-   from config import *
+    from config import *
 except ImportError:
-   pass
+    pass
 
-#Database connection
-mysql_db = peewee.MySQLDatabase(config["DATABASE_NAME"] , host=config["DATABASE_HOST"] , port=config["DATABASE_PORT"] , user=config["DATABASE_USER"] , passwd=config["DATABASE_PASSWORD"])
+# Database connection
+mysql_db = peewee.MySQLDatabase(
+    config["DATABASE_NAME"],
+    host=config["DATABASE_HOST"],
+    port=config["DATABASE_PORT"],
+    user=config["DATABASE_USER"],
+    passwd=config["DATABASE_PASSWORD"])
 
-#Fonts Model
+
+# Fonts Model
 class fonts(peewee.Model):
     font_name = peewee.CharField()
     site_url = peewee.CharField()
@@ -26,8 +32,9 @@ class fonts(peewee.Model):
     class Meta:
         database = mysql_db
 
-#connect database
+
+# connect database
 mysql_db.connect()
 
-#create table if not exist
+# create table if not exist
 fonts.create_table(True)
--- main/views.py
+++ main/views.py
@@ -1,94 +1,93 @@
-from flask import Blueprint, render_template , request
+from flask import Blueprint, render_template, request
 
 # For scraping
-import urllib2 , httplib
+import urllib2
+import httplib
 from BeautifulSoup import BeautifulSoup
 from urlparse import urlparse
 import cssutils
 
-from main.model import mysql_db
-
-
-
 main = Blueprint('main', __name__, template_folder='pages')
 
-fonts_junk=[]
-newlist=[]
+fonts_junk = []
+newlist = []
 
 
 @main.route('/')
 def index():
 
-    data = fetch_css( "http://walletkit.com/" )
+    data = fetch_css("http://walletkit.com/")
     return render_template('index.html')
 
 
-@main.route('/search' , methods=['POST'])
+@main.route('/search', methods=['POST'])
 def search(data=None):
 
     if request.method == 'POST':
-      searchq = request.form['searchquery']
+        searchq = request.form['searchquery']
 
-    return render_template('search_results.html',data=searchq)
+    return render_template('search_results.html', data=searchq)
+
 
 @main.route('/addnew')
 def addnew():
 
     return render_template('addnew.html')
 
+
 def fetch_css(url):
 
     try:
-      response = urllib2.urlopen(url)
-      html_data = response.read()
-      response.close()
+        response = urllib2.urlopen(url)
+        html_data = response.read()
+        response.close()
 
-      soup = BeautifulSoup(''.join(html_data))
+        soup = BeautifulSoup(''.join(html_data))
 
-      # Find all external style sheet references
-      ext_styles = soup.findAll('link', rel="stylesheet")
+        # Find all external style sheet references
+        ext_styles = soup.findAll('link', rel="stylesheet")
 
-      # Loop through all the found ext stylesheet, extract the relative URL,
-      # append the base URL, and fetch all content in that URL
-      # first, check to see if there are any results within ext_styles.
-      ext_css_data = ''
-      ext_found = 1
-      if len(ext_styles) != 0:
-        for i in ext_styles:
-            # Check to see if the href to css style is absolute or relative
-            o = urlparse(i['href'])
-            if o.scheme == "":
-               css_url = url + '/' + i['href']  # added "/" just in case
-               print "Found external stylesheet: " + css_url
-            else:
-               css_url = i['href']
-               print "Found external stylesheet: " + css_url
+        # Loop through all the found ext stylesheet, extract the relative URL,
+        # append the base URL, and fetch all content in that URL
+        # first, check to see if there are any results within ext_styles.
+        ext_css_data = ''
+        ext_found = 1
+        if len(ext_styles) != 0:
+            for i in ext_styles:
+                # Check to see if the href to css style is absolute or relative
+                o = urlparse(i['href'])
+                if o.scheme == "":
+                    css_url = url + '/' + i['href']  # added "/" just in case
+                    print "Found external stylesheet: " + css_url
+                else:
+                    css_url = i['href']
+                    print "Found external stylesheet: " + css_url
 
-            response = urllib2.urlopen(css_url)
-            ext_css_data += response.read()
-            response.close()
-      else:
-         ext_found = 0
-         print "No external stylesheets found"
-      all_css_data = unicode(ext_css_data, errors='ignore')
+                response = urllib2.urlopen(css_url)
+                ext_css_data += response.read()
+                response.close()
+        else:
+            ext_found = 0
+            print "No external stylesheets found"
+        all_css_data = unicode(ext_css_data, errors='ignore')
 
-      sheet = cssutils.parseString(all_css_data)
+        sheet = cssutils.parseString(all_css_data)
 
-      ## CSS PARSING ##
-      for rule in sheet:
-        if rule.type == rule.STYLE_RULE:
+        # CSS PARSING ##
+        for rule in sheet:
+            if rule.type == rule.STYLE_RULE:
 
-          # find property
-          for property in rule.style:
-            if property.name == 'font-family':
-              fonts_junk.append(str(property.value))
+                # find property
+                for property in rule.style:
+                    if property.name == 'font-family':
+                        fonts_junk.append(str(property.value))
 
-              # remove duplicate entries
-              for i in fonts_junk:
-                if i not in newlist:
-                  newlist.append(i)
+                        # remove duplicate entries
+                        for i in fonts_junk:
+                            if i not in newlist:
+                                newlist.append(i)
 
-      return newlist
+        return newlist
 
     except:
-        return "",0,0
+        return "", 0, 0
