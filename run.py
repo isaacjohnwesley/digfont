c80711e3cd7e584a57900fda5d12da79961e1a44
Sanket Saurav <sanketsaurav@gmail.com>
2013-11-15 00:10:00 +0530

    Fixed a bullcrap.

--- run.py
+++ run.py
@@ -37,4 +37,4 @@ app.config.from_pyfile('settings.py')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='localhost.com', port=port, debug= True)
+    app.run(host='0.0.0.0', port=port, debug= True)
