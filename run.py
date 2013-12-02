ee194f08b061aa1449641099a2412b71373bf917
kracekumar <me@kracekumar.com>
2013-12-02 16:56:55 +0530

    Removed setting file from top level directory

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-from main import app, db
-
-print app.__dict__
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug= True)
-    print app.routes
